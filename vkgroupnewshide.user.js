b611dc6c61b01f46af13504242a8737b702bd351
Kravtsov Nikolai <vampirus.666@gmail.com>
2012-01-29 11:25:58 +0900

    hidding on scrolling

--- vkgroupnewshide.user.js
+++ vkgroupnewshide.user.js
@@ -3,32 +3,38 @@
 // @name           Hide repost group for vkontakte.ru
 // @description    Hide records from the groups that were shared by your friends on vkontakte.ru
 // @namespace      http://vampirus.ru
-// @include        http://vkontakte.ru/feed
-// @include        http://vk.com/feed
-// @version        0.0.1
+// @include        http://vk.com/feed*
+// @version        0.0.2
 // @licence        LGPL 3
 // ==/UserScript==
 
 (function(){
-	//var w = window.wrappedJSObject || window;
-	var images = document.querySelectorAll('a.published_by_photo img');
-	var reg = new RegExp('.*?/g\\d+/.*?\\.jpg');
-	for (var i in images) {
-		if(reg.test(images[i].src)) {
-			images[i].parentNode
-				.parentNode
-				.parentNode
-				.parentNode
-				.parentNode
-				.parentNode
-				.parentNode
-				.parentNode
-				.parentNode
-				.parentNode
-				.parentNode
-				.parentNode
-				.style.display='none';
+	var w = window.wrappedJSObject || window;
+	function hideLikes() {
+		var images = document.querySelectorAll('a.published_by_photo img');
+		var reg = new RegExp('.*?/g\\d+/.*?\\.jpg');
+		for (var i in images) {
+			if(reg.test(images[i].src)) {
+				images[i].parentNode
+					.parentNode
+					.parentNode
+					.parentNode
+					.parentNode
+					.parentNode
+					.parentNode
+					.parentNode
+					.parentNode
+					.parentNode
+					.style.display='none';
+			}
 		}
 	}
-	
+	var height = document.documentElement.scrollHeight;
+	hideLikes();
+	w.setInterval(function(){
+		if (height != document.documentElement.scrollHeight) {
+			height = document.documentElement.scrollHeight;
+			hideLikes();
+		}
+	},1000);
 })();
