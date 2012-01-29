fe635e454cec14416282b06c34a5adb0e606f74a
Kravtsov Nikolai <vampirus.666@gmail.com>
2012-01-29 11:37:06 +0900

    removing advertising

--- vkgroupnewshide.user.js
+++ vkgroupnewshide.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @author         VampiRUS
-// @name           Hide repost group for vkontakte.ru
-// @description    Hide records from the groups that were shared by your friends on vkontakte.ru
+// @name           Hide repost group for vk.com
+// @description    Hide records from the groups that were shared by your friends on vk.com and advertising
 // @namespace      http://vampirus.ru
 // @include        http://vk.com/feed*
-// @version        0.0.2
+// @version        0.0.3
 // @licence        LGPL 3
 // ==/UserScript==
 
@@ -30,6 +30,7 @@
 		}
 	}
 	var height = document.documentElement.scrollHeight;
+	document.getElementById('left_ads').style.display='none';
 	hideLikes();
 	w.setInterval(function(){
 		if (height != document.documentElement.scrollHeight) {
