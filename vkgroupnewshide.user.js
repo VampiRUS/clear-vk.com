e7eed7e65160c9f81a9b1adefcfeea14af7880fc
Kravtsov Nikolai <vampirus.666@gmail.com>
2012-01-29 10:04:29 +0900

    first commit

---
+++ vkgroupnewshide.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @author         VampiRUS
+// @name           Hide repost group for vkontakte.ru
+// @description    Hide records from the groups that were shared by your friends on vkontakte.ru
+// @namespace      http://vampirus.ru
+// @include        http://vkontakte.ru/feed
+// @include        http://vk.com/feed
+// @version        0.0.1
+// @licence        LGPL 3
+// ==/UserScript==
+
+(function(){
+	//var w = window.wrappedJSObject || window;
+	var images = document.querySelectorAll('a.published_by_photo img');
+	var reg = new RegExp('.*?/g\\d+/.*?\\.jpg');
+	for (var i in images) {
+		if(reg.test(images[i].src)) {
+			images[i].parentNode
+				.parentNode
+				.parentNode
+				.parentNode
+				.parentNode
+				.parentNode
+				.parentNode
+				.parentNode
+				.parentNode
+				.parentNode
+				.parentNode
+				.parentNode
+				.style.display='none';
+		}
+	}
+	
+})();
