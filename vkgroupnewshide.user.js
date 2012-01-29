a507ad199f95323b65d2a4d6a3baf44c0e143db7
Kravtsov Nikolai <vampirus.666@gmail.com>
2012-01-29 12:20:15 +0900

    fix removing news on self page

--- vkgroupnewshide.user.js
+++ vkgroupnewshide.user.js
@@ -4,41 +4,41 @@
 // @description    Hide records from the groups that were shared by your friends on vk.com and advertising
 // @namespace      http://vampirus.ru
 // @include        http://vk.com/*
-// @version        0.0.3
+// @version        0.0.4
 // @licence        LGPL 3
 // ==/UserScript==
 
 (function(){
 	var w = window.wrappedJSObject || window;
 	function hideLikes() {
-		var images = document.querySelectorAll('a.published_by_photo img');
-		var reg = new RegExp('.*?/g\\d+/.*?\\.jpg');
-		for (var i in images) {
-			if(reg.test(images[i].src)) {
-				images[i].parentNode
-					.parentNode
-					.parentNode
-					.parentNode
-					.parentNode
-					.parentNode
-					.parentNode
-					.parentNode
-					.parentNode
-					.parentNode
-					.style.display='none';
+		if (/http:\/\/vk.com\/feed*/.test(document.location.href)) {
+			var images = document.querySelectorAll('a.published_by_photo img');
+			var reg = new RegExp('.*?/g\\d+/.*?\\.jpg');
+			for (var i in images) {
+				if(reg.test(images[i].src)) {
+					images[i].parentNode
+						.parentNode
+						.parentNode
+						.parentNode
+						.parentNode
+						.parentNode
+						.parentNode
+						.parentNode
+						.parentNode
+						.parentNode
+						.style.display='none';
+				}
 			}
 		}
 	}
 	var height = document.documentElement.scrollHeight;
 	var adv = document.getElementById('left_ads');
 	if (adv) adv.style.display='none';
-	if (/http:\/\/vk.com\/feed*/.test(document.location.href)) {
-		hideLikes();
-		w.setInterval(function(){
-			if (height != document.documentElement.scrollHeight) {
-				height = document.documentElement.scrollHeight;
-				hideLikes();
-			}
-		},1000);
-	}
+	hideLikes();
+	w.setInterval(function(){
+		if (height != document.documentElement.scrollHeight) {
+			height = document.documentElement.scrollHeight;
+			hideLikes();
+		}
+	},1000);
 })();
